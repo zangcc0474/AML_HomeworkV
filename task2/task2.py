f2d1c29dbb3349781033030e5fa30e4da33e62bf
ChenchaoZang <cz2431@columbia.edu>
2017-04-29 13:52:28 -0400

    edit task2

--- task2/task2.py
+++ task2/task2.py
@@ -41,8 +41,30 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 vanila = model.fit(X_train, y_train, batch_size=128, epochs=20, verbose=1, validation_split=.1)
 
 
-from keras.layers import Dropout
+def make_model_vanila(optimizer="adam", hidden_size=32):
+    model = Sequential([
+        Dense(32, input_shape=(784,)),
+        Activation('relu'),
+        Dense(10),
+        Activation('softmax'),
+    ])
+    model.compile("adam", "categorical_crossentropy", metrics=['accuracy'])
+    return model
 
+
+clf_vanila = KerasClassifier(make_model_vanila)
+param_grid = {'epochs': [1, 5, 10],  # epochs is fit parameter, not in make_model!
+              'hidden_size': [32, 64, 256]}
+grid_vanila = GridSearchCV(clf_vanila, param_grid=param_grid, cv=5)
+grid_vanila.fit(X_train, y_train)
+
+
+res_vanila = pd.DataFrame(grid_vanila.cv_results_)
+res_vanila.pivot_table(index=["param_epochs", "param_hidden_size"],
+                values=['mean_train_score', "mean_test_score"])
+
+
+from keras.layers import Dropout
 model_dropout = Sequential([
     Dense(1024, input_shape=(784,), activation='relu'),
     Dropout(.5),
@@ -53,6 +75,30 @@ model_dropout = Sequential([
 model_dropout.compile("adam", "categorical_crossentropy", metrics=['accuracy'])
 history_dropout = model_dropout.fit(X_train, y_train, batch_size=128,
                             epochs=20, verbose=1, validation_split=.1)
+
+
+def make_model_batch(optimizer="adam", hidden_size=32):
+    model = Sequential([
+        Dense(32, input_shape=(784,)),
+        Activation('relu'),
+        Dense(10),
+        Activation('softmax'),
+    ])
+    model.compile("adam", "categorical_crossentropy", metrics=['accuracy'])
+    return model
+
+
+clf_batch = KerasClassifier(make_model_vanila)
+param_grid = {'epochs': [1, 5, 10],  # epochs is fit parameter, not in make_model!
+              'hidden_size': [32, 64, 256]}
+grid_batch = GridSearchCV(clf_batch, param_grid=param_grid, cv=5)
+grid_batch.fit(X_train, y_train)
+
+
+res_batch = pd.DataFrame(grid_batch.cv_results_)
+res_batch.pivot_table(index=["param_epochs", "param_hidden_size"],
+                values=['mean_train_score', "mean_test_score"])
+
 
 df_vanila = pd.DataFrame(vanila.history)
 df_vanila[['acc', 'val_acc']].plot()
