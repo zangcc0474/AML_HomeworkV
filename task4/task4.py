6f8f2b202b61612ec4974572858fafbf1a22fb5f
ChenchaoZang <cz2431@columbia.edu>
2017-04-29 14:02:38 -0400

    edit file4

--- task4/task4.py
+++ task4/task4.py
@@ -2,7 +2,7 @@ import os
 from keras.preprocessing import image
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 from keras.applications.vgg16 import preprocess_input
 from keras.preprocessing import image
 from keras import applications
