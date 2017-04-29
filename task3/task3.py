f35f751f28b06f63ba6fb97829e3443d8061a6b7
ChenchaoZang <cz2431@columbia.edu>
2017-04-29 00:42:43 -0400

    modify task3 again again

--- task3/task3.py
+++ task3/task3.py
@@ -57,7 +57,7 @@ cnn.add(Dense(num_classes, activation='softmax'))
 
 cnn.compile("adam", "categorical_crossentropy", metrics=['accuracy'])
 history_cnn = cnn.fit(x_train, y_train_binary,
-                      batch_size = 200, epochs=50, verbose=1, validation_split=.2)
+                      batch_size = 200, epochs=30, verbose=1, validation_split=.2)
 
 
 print(cnn.evaluate(x_test, y_test_xbinary))
@@ -79,6 +79,6 @@ cnn_small_bn.add(Dense(64, activation='relu'))
 cnn_small_bn.add(Dense(num_classes, activation='softmax'))
 cnn_small_bn.compile("adam", "categorical_crossentropy", metrics=['accuracy'])
 history_cnn_small_bn = cnn_small_bn.fit(x_train, y_train_binary,
-                                        batch_size=200, epochs=50, verbose=1, validation_split=.2)
+                                        batch_size=200, epochs=30, verbose=1, validation_split=.2)
 
 print(cnn_small_bn.evaluate(x_test,y_test_xbinary))
